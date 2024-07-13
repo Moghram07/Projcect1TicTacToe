00f6526b120623105773f3857265a1523220e5c6
OMAR ALSHEHRI <166976917+Moghram07@users.noreply.github.com>
2024-07-14 02:11:00 +0300

    Update Main.java
    adding the extra credit tasks and changing colors of 'X' and 'O' to improve the user experience.

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,85 +6,129 @@ public class Main {
         char[][] gameBoard = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
         char user = 'X';
         char computer = 'O';
-        int squarePlayed = 0;
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to Tic Tac Toe");
         printBoard(gameBoard);
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        while (squarePlayed < 9) {
-            int userInput=getUserInput(scanner, gameBoard);//user turn
-            gamePlay(gameBoard, userInput, user);
-            printBoard(gameBoard);
-            squarePlayed++;
-            if(checkWin(gameBoard, user)){
-                System.out.println("Congratulations! you won!");
-                break;
+
+        System.out.println("Please choose the game mode:");
+        System.out.println("1 - One round to win");
+        System.out.println("2 - Three rounds to win");
+        int choice = scanner.nextInt();
+
+        int roundsToWin = (choice == 1) ? 1 : 3;
+        int userWins = 0;
+        int computerWins = 0;
+
+        while (userWins < roundsToWin && computerWins < roundsToWin) {
+            int squarePlayed = 0;
+            while (squarePlayed < 9) {
+                int userInput = getUserInput(scanner, gameBoard, user);
+                gamePlayAndValidate(gameBoard, userInput, user);
+                printBoard(gameBoard);
+                squarePlayed++;
+                if (checkWin(gameBoard, user)) {
+                    System.out.println("Congratulations! You won this round.");
+                    userWins++;
+                    break;
+                }
+
+                int computerInput;
+                Random random = new Random();
+                do {
+                    computerInput = random.nextInt(9) + 1;
+                } while (!gamePlayAndValidate(gameBoard, computerInput, computer));
+                System.out.println("Computer chooses square number " + computerInput);
+                printBoard(gameBoard);
+                squarePlayed++;
+                if (checkWin(gameBoard, computer)) {
+                    System.out.println("Computer wins this round. Better luck next time!");
+                    computerWins++;
+                    break;
+                } else if (squarePlayed == 9) {
+                    System.out.println("The game is a tie!");
+                }
             }
-            int computerInput; // now the computer turn
-            do{
-                computerInput = random.nextInt(9) + 1;
-            } while(!isValid(gameBoard,computerInput));
-            gamePlay(gameBoard, computerInput, computer);
-            System.out.println("Computer chooses square number" + computerInput);
-            printBoard(gameBoard);
-            squarePlayed++;
-            if (checkWin(gameBoard, computer)){
-                System.out.println("Computer wins! buy a new computer.");
-                break;
-            }
+
+            // Reset the game board for the next round
+            resetBoard(gameBoard);
+            System.out.println("Scores: User - " + userWins + ", Computer - " + computerWins);
         }
-        if(squarePlayed == 9 && !checkWin(gameBoard, user) && !checkWin(gameBoard, computer)){
-            System.out.println("The game is a tie!");
+
+        if (userWins > computerWins) {
+            System.out.println("Congratulations! You won the game!");
+        } else if (computerWins > userWins) {
+            System.out.println("Computer wins the game. Better luck next time!");
+        } else {
+            System.out.println("The game ends in a tie!");
         }
+
+        scanner.close();
     }
+
     public static void printBoard(char[][] board) {
         System.out.println(" |===|==|===|==|===|");
-        for (char[] a : board) {
-            for (char b : a) {
-                System.out.print(" | " + b + " | ");
+        for (char[] row : board) {
+            for (char cell : row) {
+                if (cell == 'X') {
+                    System.out.print(" | \u001B[31mX\u001B[0m | "); // Red color for 'X'
+                } else if (cell == 'O') {
+                    System.out.print(" | \u001B[34mO\u001B[0m | "); // Blue color for 'O'
+                } else {
+                    System.out.print(" | " + cell + " | ");
+                }
             }
             System.out.println("\n |===|==|===|==|===|");
         }
     }
-    public static boolean isValid(char[][] board, int input) {
+
+    public static boolean gamePlayAndValidate(char[][] board, int input, char player) {
         int row = (input - 1) / 3;
         int col = (input - 1) % 3;
-        return Character.isDigit(board[row][col]);
+        if (Character.isDigit(board[row][col])) {
+            board[row][col] = player;
+            return true; // Valid move
+        } else {
+            return false; // Invalid move
+        }
     }
-    public static void gamePlay(char[][] board, int input, char player) {
-        int row = (input - 1) / 3;
-        int col = (input - 1) % 3;
-        board[row][col] = player;
-    }
-    public static int getUserInput(Scanner scanner, char[][]board){
+
+    public static int getUserInput(Scanner scanner, char[][] board, char player) {
         int input;
-        while(true){
+        while (true) {
             System.out.println("Enter a number from (1-9): ");
             input = scanner.nextInt();
-            if(isValid(board, input)){
-                break;
-            }else{
-                System.out.println("Invalid input. Please choose an available square number.");
+            if (input < 1 || input > 9) {
+                System.out.println("Invalid input. Please choose a number between 1 and 9.");
+                continue;
+            }
+            int row = (input - 1) / 3;
+            int col = (input - 1) % 3;
+            if (Character.isDigit(board[row][col])) {
+                return input;
+            } else {
+                System.out.println("Invalid input. The square is already taken.");
             }
         }
-        return input;
     }
-    public static boolean checkWin(char[][] board, char marker){
-        for(int i =0; i<3; i++) {
-            if (board[i][0] == marker && board[i][1] == marker && board[i][2] == marker) {
-                return true;
-            }
-        }
-        for(int j =0; j<3; j++) {
-            if (board[0][j] == marker && board[1][j] == marker && board[2][j] == marker) {
-                return true;
-            }
-        }
-            if((board[0][0]==marker&&board[1][1]==marker&&board[2][2]==marker)||
-                    (board[0][2]==marker&&board[1][1]==marker&&board[2][0]==marker)){
-                return true;
-            }
 
-        return false;
+    public static boolean checkWin(char[][] board, char marker) {
+        // Check rows, columns, and diagonals for a win
+        return (board[0][0] == marker && board[0][1] == marker && board[0][2] == marker) ||
+                (board[1][0] == marker && board[1][1] == marker && board[1][2] == marker) ||
+                (board[2][0] == marker && board[2][1] == marker && board[2][2] == marker) ||
+                (board[0][0] == marker && board[1][0] == marker && board[2][0] == marker) ||
+                (board[0][1] == marker && board[1][1] == marker && board[2][1] == marker) ||
+                (board[0][2] == marker && board[1][2] == marker && board[2][2] == marker) ||
+                (board[0][0] == marker && board[1][1] == marker && board[2][2] == marker) ||
+                (board[0][2] == marker && board[1][1] == marker && board[2][0] == marker);
+    }
+
+    public static void resetBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = (char) ('1' + i * board.length + j);
+            }
+        }
     }
 }
