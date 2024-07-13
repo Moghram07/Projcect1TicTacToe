241416b550ac72c2b8870cce7a7418cefe5e2b52
OMAR ALSHEHRI <166976917+Moghram07@users.noreply.github.com>
2024-07-13 21:42:19 +0300

    Update Main.java
    28 lines shorter than the previous code

--- Main.java
+++ Main.java
@@ -1,121 +1,92 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        char[][] gameBoard = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+        char user = 'X';
+        char computer = 'O';
+        int squarePlayed = 0;
+        System.out.println("Welcome to Tic Tac Toe");
+        printBoard(gameBoard);
         Scanner scanner = new Scanner(System.in);
-
-        char[][] board = {
-                {'1', '2', '3'},
-                {'4', '5', '6'},
-                {'7', '8', '9'}
-        };
-        int squarePlayed = 0; //track the number of played squares
-        char userTurn = 'X';  //the user will be X
-        char computerTurn = 'O';
-        String endMessage = "Game Over";
-
+        Random random = new Random();
         while (squarePlayed < 9) {
-            gameBoard(board);
-            //User's turn
-            if (squarePlayed % 2 == 0) {
-                handleMove(board, scanner, userTurn);
-            } else {
-                //Computer's turn
-                handleMove(board, null, computerTurn); // Passing null for scanner as computer doesn't use it
-            }
+            int userInput=getUserInput(scanner, gameBoard);//user turn
+            gamePlay(gameBoard, userInput, user);
+            printBoard(gameBoard);
             squarePlayed++;
-            //Check for win after each move
-            if (checkWin(board, userTurn)) {
-                gameBoard(board);
-                System.out.println("Congratulations! You win!");
+            if(checkWin(gameBoard, user)){
+                System.out.println("Congratulations! you won!");
                 break;
-            } else if (checkWin(board, computerTurn)) {
-                gameBoard(board);
-                System.out.println("Computer wins!");
+            }
+            int computerInput; // now the computer turn
+            do{
+                computerInput = random.nextInt(9) + 1;
+            } while(!isValid(gameBoard,computerInput));
+            gamePlay(gameBoard, computerInput, computer);
+            System.out.println("Computer chooses square number" + computerInput);
+            printBoard(gameBoard);
+            squarePlayed++;
+            if (checkWin(gameBoard, computer)){
+                System.out.println("Computer wins! buy a new computer.");
                 break;
             }
         }
-        if (squarePlayed == 9 && !checkWin(board, userTurn) && !checkWin(board, computerTurn)) {
-            gameBoard(board);
+        if(squarePlayed == 9 && !checkWin(gameBoard, user) && !checkWin(gameBoard, computer)){
             System.out.println("The game is a tie!");
         }
-        System.out.println(endMessage);
-        scanner.close();
     }
-    //Method to handle user and computer move
-    public static void handleMove(char[][] board, Scanner scanner, char turn) {
-        if (turn == 'X') {
-            System.out.println("Your turn. Choose a square number (1-9): ");
-            int input = getUserInput(scanner);
-            updateBoard(board, input, turn);
-        } else {
-            //Computer's turn
-            int computerMove = getComputerMove(board);
-            updateBoard(board, computerMove, turn);
-            System.out.println("Computer chooses square number " + computerMove);
+    public static void printBoard(char[][] board) {
+        System.out.println(" |===|==|===|==|===|");
+        for (char[] a : board) {
+            for (char b : a) {
+                System.out.print(" | " + b + " | ");
+            }
+            System.out.println("\n |===|==|===|==|===|");
         }
     }
-
-    public static void updateBoard(char[][] board, int position, char marker) {
-        int row = (position - 1) / 3; //row index
-        int col = (position - 1) % 3; //column index
-        board[row][col] = marker; // update board
+    public static boolean isValid(char[][] board, int input) {
+        int row = (input - 1) / 3;
+        int col = (input - 1) % 3;
+        return Character.isDigit(board[row][col]);
     }
-
-    public static void gameBoard(char[][] board) {
-        System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
-        System.out.println("- + - + -");
-        System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
-        System.out.println("- + - + -");
-        System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
-        System.out.println();
+    public static void gamePlay(char[][] board, int input, char player) {
+        int row = (input - 1) / 3;
+        int col = (input - 1) % 3;
+        board[row][col] = player;
     }
-
-    public static int getUserInput(Scanner scanner) {
-        while (true) {
-            int input = scanner.nextInt();
-            if (input >= 1 && input <= 9) {
-                return input;
-            } else {
-                System.out.println("Invalid input. Please choose a number between 1 and 9.");
+    public static int getUserInput(Scanner scanner, char[][]board){
+        int input;
+        while(true){
+            System.out.println("Enter a number from (1-9): ");
+            input = scanner.nextInt();
+            if(isValid(board, input)){
+                break;
+            }else{
+                System.out.println("Invalid input. Please choose an available square number.");
             }
         }
+        return input;
     }
-    //generate a random number for the computer input
-    public static int getComputerMove(char[][] board) {
-        Random rand = new Random();
-        int move;
-        do {
-            move = rand.nextInt(9) + 1;
-        } while (!isValidMove(board, move));
-        return move;
-    }
-    //method to check if the chosen number is valid
-    public static boolean isValidMove(char[][] board, int position) {
-        int row = (position - 1) / 3;
-        int col = (position - 1) % 3;
-        return board[row][col] != 'X' && board[row][col] != 'O';
-    }
-    //method to check who won the game
-    public static boolean checkWin(char[][] board, char marker) {
-        //Check rows
-        for (int i = 0; i < 3; i++) {
+    public static boolean checkWin(char[][] board, char marker){
+        for(int i =0; i<3; i++) {
             if (board[i][0] == marker && board[i][1] == marker && board[i][2] == marker) {
                 return true;
             }
         }
-        //Check columns
-        for (int j = 0; j < 3; j++) {
+        for(int j =0; j<3; j++) {
             if (board[0][j] == marker && board[1][j] == marker && board[2][j] == marker) {
                 return true;
             }
         }
-        //Check diagonals
-        if ((board[0][0] == marker && board[1][1] == marker && board[2][2] == marker) ||
-                (board[0][2] == marker && board[1][1] == marker && board[2][0] == marker)) {
-            return true;
-        }
+            if((board[0][0]==marker&&board[1][1]==marker&&board[2][2]==marker)||
+                    (board[0][2]==marker&&board[1][1]==marker&&board[2][0]==marker)){
+                return true;
+            }
+
         return false;
     }
 }
